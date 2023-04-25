0fa6063763fa9da55d1764d2bce7ee47e4b052df
Wojtek-Rokicki <rokwojtek@gmail.com>
2023-04-25 10:06:56 +0200

    Web forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from app.config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# There are serveral ways to configure your app:
+# app.config['SECRET_KEY'] = 'you-will-never-guess'
+# The other one, which is more separation of concerns alike:
+# In config.py: Secret key configuration
+# import os
+# class Config(object):
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+# The Flask-WTF extension uses SECRET_KEY to protect web forms against a nasty attack called Cross-Site Request Forgery
+# The other use of this variable is for cryptographic purposes
 
 from app import routes
